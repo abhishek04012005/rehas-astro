c45e2d069595e53b63d6a478f1071126ec628b96
abhishek04012005 <abhishekchoudhary5414@gmail.com>
2026-07-15 23:39:19 +0530

    fdsaf

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -26,7 +26,7 @@ export function Navbar() {
       <div className={`shell ${styles.shellNav}`}>
         <Link href="/" className={styles.brand}>
           <span className={styles.brandMark}>
-            <Image src={LogoImage} alt="REHAS logo" width={120} height={80} />
+            <Image src={LogoImage} alt="REHAS logo" width={200} height={80} />
           </span>
         </Link>
 
